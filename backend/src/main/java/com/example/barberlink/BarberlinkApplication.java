8e7af116ef5f44cd38ada272da3cfa8d0e8dfde9
ManuDev <vargas_manuel08@hotmail.com>
2025-02-18 19:31:00 -0500

    feat: estructura del backend incializado

---
+++ backend/src/main/java/com/example/barberlink/BarberlinkApplication.java
@@ -0,0 +1,13 @@
+package com.example.barberlink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarberlinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BarberlinkApplication.class, args);
+	}
+
+}
