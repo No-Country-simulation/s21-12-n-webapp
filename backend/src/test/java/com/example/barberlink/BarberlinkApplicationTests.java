12b6714351b0bb6039869d56e6755c7417b697af
JesusMedina21 <jesusmedina0921@gmail.com>
2025-02-22 19:43:58 -0400

    backend

---
+++ backend/src/test/java/com/example/barberlink/BarberlinkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.barberlink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BarberlinkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
