d8d29a888929f4b50089421b46167e70a3c306a6
Jeremias Rivelli <jeremias.j.riv@gmail.com>
2025-03-12 00:01:23 -0300

    Cambios de seguridad

--- backend/src/main/java/com/barberlink/mapper/request/ClienteRequest.java
+++ backend/src/main/java/com/barberlink/mapper/request/ClienteRequest.java
@@ -1,5 +1,8 @@
 package com.barberlink.mapper.request;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
 public record ClienteRequest(
         String email,
         String contrasena,
