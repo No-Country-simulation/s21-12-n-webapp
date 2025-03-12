1196e857c6aa98696bbdb00b79d6055dc481fe83
Jeremias Rivelli <jeremias.j.riv@gmail.com>
2025-03-11 22:39:28 -0300

    Cambios y unificacion

--- backend/src/main/java/com/barberlink/infra/configuracion/ConfiguracionCors.java
+++ backend/src/main/java/com/barberlink/infra/configuracion/ConfiguracionCors.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
 import java.util.List;
 
 @Configuration
@@ -14,9 +15,10 @@ public class ConfiguracionCors {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(List.of(
-            "http://localhost:4200", 
-            "http://localhost:5173"  
-    ));
+                "http://localhost:4200",
+                "http://localhost:5173",
+                "https://jereprograma.com"
+        ));
         configuration.setAllowedMethods(List.of("GET", "POST", "OPTIONS", "PUT", "DELETE", "PATCH"));
         configuration.setAllowedHeaders(List.of("Authorization", "Content-Type", "Accept"));
         configuration.addExposedHeader("Authorization");
